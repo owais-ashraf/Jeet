1118cfcee5f8c8b55405f528b77dff4c87b934fc
owais-ashraf <65316921+owais-ashraf@users.noreply.github.com>
2023-03-31 06:24:13 +0530

    Add files via upload

---
+++ Assignments/7/script.js
@@ -0,0 +1,12 @@
+// var names = ["Ali", "Asad","Sadaf","Saad","das","fahad","fasi","adnan","jamal","kali"]
+// console.log(names)
+
+
+let myArray = ["Toyota","Honda","BMW","Nissan","Audi","Isuzu","Tata","Kia"]
+function arr(myArray){
+    return myArray;
+}
+var cars = arr(myArray)
+console.log(cars)
+
+
