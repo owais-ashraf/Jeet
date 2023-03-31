bf854c79027e34d61350c72afbe5f7caca27bd18
owais-ashraf <owais.ashraf72@gmail.com>
2023-03-31 06:51:54 +0530

    sum added

--- Assignments/7/tempCodeRunnerFile.js
+++ Assignments/7/tempCodeRunnerFile.js
@@ -8,13 +8,3 @@ function arr(myArray){
 }
 var cars = arr(myArray)
 console.log(cars)
-
-
-function sumOfNum(arr){
-    let sum = 0;
-    for(let a of arr)
-        sum += a;
-        return sum;
-    
-}
-console.log(sumOfNum([2,3,4]))
